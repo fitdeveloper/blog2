08ad169fcee1fee2cf1d289df68a13f5d0847829
ZINE Mahmoud <mr.zine.mahmoud@gmail.com>
2019-03-24 13:34:55 +0100

    creation des data array posts danns l'app Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog2';
+  titleApp = 'blog2';
+  
+  posts = [
+    {
+      title : 'post 1',
+      content : 'content 1 content 1',
+      loveIts : 0,
+      created_at: new Date()
+    },
+    {
+      title : 'post 2',
+      content : 'content 2 content 2',
+      loveIts : 6,
+      created_at: new Date()
+    },
+    {
+      title : 'post 3',
+      content : 'content 3 content 3',
+      loveIts : -3,
+      created_at: new Date()
+    }
+
+  ];
 }
